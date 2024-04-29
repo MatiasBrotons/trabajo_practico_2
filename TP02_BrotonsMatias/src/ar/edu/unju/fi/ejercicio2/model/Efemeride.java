ce862d2075bb8ccd40996d5b579a0ebb1acf63bf
PC <PC@MATIAS>
2024-04-29 20:21:55 -0300

    Creacion de 2da rama, paquetes y clases

---
+++ TP02_BrotonsMatias/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio2.model;
+
+import ar.edu.unju.fi.ejercicio2.constantes.*;
+
+public class Efemeride {
+		private String codigos;
+		private Mes mes;
+		private double dia;
+		private String detalle;
+}
