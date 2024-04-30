9cd7034586bbca383509f59c850adfa970325044
PC <PC@MATIAS>
2024-04-29 21:26:21 -0300

    Ejercicio3

--- TP02_BrotonsMatias/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ TP02_BrotonsMatias/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -1,9 +1,20 @@
 package ar.edu.unju.fi.ejercicio3.main;
 
+import ar.edu.unju.fi.ejercicio3.constantes.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Provincia[] provincias = Provincia.values();
 
-	public static void main(String[] args) {
-
-	}
-
+        for (Provincia provincia : provincias) {
+            System.out.println("Provincia: " + provincia);
+            System.out.println("Cantidad de población: " + provincia.getCantidadPoblacion());
+            System.out.println("Superficie: " + provincia.getSuperficie() + " km²");
+            System.out.println("Densidad poblacional: " + provincia.calcularDensidadPoblacional() + " habitantes/km²");
+            System.out.println();
+        }
+    }
 }
+
+
+
